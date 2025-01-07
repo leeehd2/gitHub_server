9c53d706814821d51b13d85e1812aee6d36a0d57
leedong <leeehd@nate.com>
2025-01-07 14:18:38 +0900

    git test 01.07

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # 자주 사용하는 값 변수에 저장
 REPOSITORY=/home/ec2-user/gitHub_server
 PROJECT_NAME=server-all
+JARDIR=/home/ec2-user
 
 # git clone 받은 위치로 이동
 #cd $REPOSITORY/$PROJECT_NAME/
@@ -23,7 +24,7 @@ cd $REPOSITORY
 # build의 결과물 (jar 파일) 특정 위치로 복사
 echo "> build 파일 복사"
 #cp $REPOSITORY/build/libs/$PROJECT_NAME.jar $REPOSITORY/
-cp $REPOSITORY/build/libs/${PROJECT_NAME}.jar $REPOSITORY/
+cp $REPOSITORY/build/libs/${PROJECT_NAME}.jar $JARDIR/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.jar)
@@ -41,4 +42,4 @@ echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 
 echo "> Jar Name: $JAR_NAME"
-nohup java -jar $REPOSITORY/$JAR_NAME >>  server.log &
+nohup java -jar $JARDIR/$JAR_NAME >>  $JARDIR/LOGS/server.log &
