ff6402288db6f137d2bd154976a2430eab48a092
leedong <leeehd@nate.com>
2025-01-15 20:36:12 +0900

    git test 01.15  jar 갱신

--- src/main/kotlin/com/ld/server/api/router/UserSearch.kt
+++ src/main/kotlin/com/ld/server/api/router/UserSearch.kt
@@ -79,7 +79,7 @@ import java.sql.ResultSet
 
 
 
-        post("/usersSignUp") {
+        get("/usersSignUp") {
             println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@22")
             val userid = call.parameters["userid"]
             val password = call.parameters["password"]
